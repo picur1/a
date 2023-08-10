502e1064a682f6458e1eefbe0f2f44987e4d4b73
picur1 <133989406+picur1@users.noreply.github.com>
2023-08-10 13:50:30 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 music.play(music.stringPlayable("C5 A B G A F - - ", 1800), music.PlaybackMode.UntilDone)
 basic.forever(function () {
     basic.showLeds(`
-        . . . . .
-        . . . . .
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
         . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
         . . . . .
         . . . . .
         `)
+    basic.pause(500)
 })
